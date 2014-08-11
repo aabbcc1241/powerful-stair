af4f34e7dae441f47aba6fb2d522d34390635f9a
Beeno Tung <abbcc1241@yahoo.com.hk>
2014-08-11 15:17:10 +0800

    testing nfc

--- gen/com/example/demonew/R.java
+++ gen/com/example/demonew/R.java
@@ -34,7 +34,7 @@ public final class R {
     public static final class id {
         public static final int FrameLayout1=0x7f080000;
         public static final int LinearLayout1=0x7f08000a;
-        public static final int action_settings=0x7f080015;
+        public static final int action_settings=0x7f08001a;
         public static final int button1=0x7f080004;
         public static final int button2=0x7f080005;
         public static final int button3=0x7f080006;
@@ -44,16 +44,21 @@ public final class R {
         public static final int button_date=0x7f08000c;
         public static final int button_my_record=0x7f080003;
         public static final int button_other_exercise=0x7f080002;
-        public static final int button_start=0x7f080013;
+        public static final int button_start=0x7f080018;
         public static final int button_walk_on_hkccs_stair=0x7f080001;
-        public static final int imageView_tap_there=0x7f080014;
-        public static final int tableRow1=0x7f08000d;
-        public static final int tableRow2=0x7f080010;
-        public static final int tableRow3=0x7f080011;
-        public static final int tableRow4=0x7f080012;
+        public static final int imageView_tap_there=0x7f080019;
+        public static final int tableRow1=0x7f08000e;
+        public static final int tableRow2=0x7f080012;
+        public static final int tableRow3=0x7f080016;
+        public static final int tableRow4=0x7f080017;
+        public static final int table_layout_my_record=0x7f08000d;
         public static final int textView_date_label=0x7f08000b;
-        public static final int textView_energy_used=0x7f08000f;
-        public static final int textView_type_of_exercise=0x7f08000e;
+        public static final int textView_energy_used=0x7f080011;
+        public static final int textView_times=0x7f080010;
+        public static final int textView_type_of_exercise=0x7f08000f;
+        public static final int tv_1=0x7f080013;
+        public static final int tv_2=0x7f080014;
+        public static final int tv_3=0x7f080015;
     }
     public static final class layout {
         public static final int activity_date_pick_action_bar=0x7f030000;
@@ -73,33 +78,41 @@ public final class R {
     }
     public static final class string {
         public static final int action_settings=0x7f050001;
-        public static final int add_record=0x7f05001b;
+        public static final int add_record=0x7f05001e;
+        public static final int alertDialogTitle=0x7f050023;
         public static final int analysis_on_personal_diet_list=0x7f050005;
         public static final int app_name=0x7f050000;
-        public static final int background_color=0x7f05000c;
-        public static final int date_default=0x7f050017;
-        public static final int date_label=0x7f050016;
-        public static final int energ_used=0x7f050019;
+        public static final int background_color=0x7f05000f;
+        public static final int date_default=0x7f05001a;
+        public static final int date_label=0x7f050019;
+        public static final int energ_used=0x7f05001c;
         public static final int energy_calculator=0x7f050006;
-        public static final int hello_world=0x7f05000e;
+        public static final int energy_per_walk_stair=0x7f05001f;
+        public static final int energy_type_hkcc_stair_walk=0x7f050020;
+        public static final int hello_world=0x7f050011;
         public static final int introduction=0x7f050004;
         public static final int loading=0x7f050009;
-        public static final int my_record=0x7f050013;
-        public static final int other_exercise=0x7f050010;
+        public static final int my_record=0x7f050016;
+        public static final int nfc_enabled=0x7f05000c;
+        public static final int nfc_not_enabled=0x7f05000b;
+        public static final int nfc_not_found=0x7f05000a;
+        public static final int ok=0x7f050022;
+        public static final int other_exercise=0x7f050013;
         public static final int personal_particular=0x7f050003;
         public static final int start=0x7f050002;
-        public static final int start_screen_slogan=0x7f05000b;
-        public static final int tap_there=0x7f050014;
+        public static final int start_screen_slogan=0x7f05000e;
+        public static final int tap_there=0x7f050017;
+        public static final int times=0x7f050021;
         public static final int tips_on_nutrition=0x7f050008;
         public static final int tips_on_physical_activities=0x7f050007;
-        public static final int title_activity_date_pick_action_bar=0x7f05001a;
-        public static final int title_activity_energy_calculator=0x7f050011;
-        public static final int title_activity_main_menu=0x7f05000a;
-        public static final int title_activity_menu_main=0x7f05000d;
-        public static final int title_activity_my_record=0x7f050015;
-        public static final int title_activity_walk_on_hkcc_stair=0x7f050012;
-        public static final int type_of_exercise=0x7f050018;
-        public static final int walk_on_hkccs_stair=0x7f05000f;
+        public static final int title_activity_date_pick_action_bar=0x7f05001d;
+        public static final int title_activity_energy_calculator=0x7f050014;
+        public static final int title_activity_main_menu=0x7f05000d;
+        public static final int title_activity_menu_main=0x7f050010;
+        public static final int title_activity_my_record=0x7f050018;
+        public static final int title_activity_walk_on_hkcc_stair=0x7f050015;
+        public static final int type_of_exercise=0x7f05001b;
+        public static final int walk_on_hkccs_stair=0x7f050012;
     }
     public static final class style {
         /** 
