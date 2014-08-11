3938e8cb9a4ecebda005b2eb38fb5fa39a610154
Beeno Tung <abbcc1241@yahoo.com.hk>
2014-08-11 11:14:04 +0800

    made main menu

--- src/com/example/demonew/StartScreenActivity.java
+++ src/com/example/demonew/StartScreenActivity.java
@@ -1,16 +1,36 @@
 package com.example.demonew;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class StartScreenActivity extends Activity {
+	Button button_start;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_start_screen);
+	}
+
+	@Override
+	protected void onResume() {
+		super.onResume();
+		button_start = (Button) findViewById(R.id.button_start);
+		button_start.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				Toast.makeText(StartScreenActivity.this, R.string.loading, Toast.LENGTH_SHORT).show();
+				Intent intent = new Intent(StartScreenActivity.this,MenuMainActivity.class);
+				StartScreenActivity.this.startActivity(intent);
+			}
+		});
 	}
 
 	@Override
