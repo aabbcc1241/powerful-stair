67e9d57048f4c0b104c0315d6f70edd018ef0ab4
Beeno Tung <abbcc1241@yahoo.com.hk>
2014-08-22 14:33:45 +0800

    completed aboutyou layout (BMI calculator)

--- gen/edu/hkcc/personalkcalmanagerhkcc/R.java
+++ gen/edu/hkcc/personalkcalmanagerhkcc/R.java
@@ -31,14 +31,20 @@ public final class R {
         public static final int navigation_drawer_width=0x7f040002;
     }
     public static final class drawable {
-        public static final int drawer_shadow=0x7f020000;
-        public static final int ic_drawer=0x7f020001;
-        public static final int ic_launcher=0x7f020002;
-        public static final int start_screen_slogan=0x7f020003;
+        public static final int button_bg_blue_1=0x7f020000;
+        public static final int button_bg_orange_1=0x7f020001;
+        public static final int button_bg_pink_1=0x7f020002;
+        public static final int drawer_shadow=0x7f020003;
+        public static final int ic_drawer=0x7f020004;
+        public static final int ic_launcher=0x7f020005;
+        public static final int start_screen_slogan=0x7f020006;
     }
     public static final class id {
-        public static final int FrameLayout1=0x7f09000e;
+        public static final int FrameLayout1=0x7f090011;
         public static final int TableLayout1=0x7f090003;
+        public static final int aboutyou_bmi=0x7f09000e;
+        public static final int aboutyou_bmi_label=0x7f09000d;
+        public static final int aboutyou_button_bmi_calc=0x7f09000c;
         public static final int aboutyou_userage=0x7f090007;
         public static final int aboutyou_userage_label=0x7f090006;
         public static final int aboutyou_userheight=0x7f090009;
@@ -47,15 +53,15 @@ public final class R {
         public static final int aboutyou_username_label=0x7f090004;
         public static final int aboutyou_userweight=0x7f09000b;
         public static final int aboutyou_userweight_label=0x7f09000a;
-        public static final int action_example=0x7f090012;
-        public static final int action_settings=0x7f090011;
+        public static final int action_example=0x7f090015;
+        public static final int action_settings=0x7f090014;
         public static final int container=0x7f090001;
         public static final int drawer_layout=0x7f090000;
-        public static final int imageView1=0x7f09000f;
+        public static final int imageView1=0x7f090012;
         public static final int navigation_drawer=0x7f090002;
-        public static final int section_label=0x7f09000d;
-        public static final int textView1=0x7f09000c;
-        public static final int welcome_button_start=0x7f090010;
+        public static final int section_label=0x7f090010;
+        public static final int textView1=0x7f09000f;
+        public static final int welcome_button_start=0x7f090013;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
@@ -70,11 +76,12 @@ public final class R {
         public static final int main=0x7f080001;
     }
     public static final class string {
-        public static final int aboutyou_bmi_label=0x7f05000d;
-        public static final int aboutyou_userage_label=0x7f05000a;
-        public static final int aboutyou_userheight_label=0x7f05000b;
-        public static final int aboutyou_username_label=0x7f050009;
-        public static final int aboutyou_userweight_label=0x7f05000c;
+        public static final int aboutyou_bmi_calc=0x7f05000f;
+        public static final int aboutyou_bmi_label=0x7f05000e;
+        public static final int aboutyou_userage_label=0x7f05000b;
+        public static final int aboutyou_userheight_label=0x7f05000c;
+        public static final int aboutyou_username_label=0x7f05000a;
+        public static final int aboutyou_userweight_label=0x7f05000d;
         public static final int action_example=0x7f050001;
         public static final int action_settings=0x7f050002;
         public static final int app_name=0x7f050000;
@@ -82,8 +89,9 @@ public final class R {
         public static final int loading=0x7f050005;
         public static final int navigation_drawer_close=0x7f050004;
         public static final int navigation_drawer_open=0x7f050003;
-        public static final int slogan=0x7f050007;
-        public static final int start=0x7f050008;
+        public static final int slogan=0x7f050008;
+        public static final int start=0x7f050009;
+        public static final int std_space_height=0x7f050007;
     }
     public static final class style {
         /** 
