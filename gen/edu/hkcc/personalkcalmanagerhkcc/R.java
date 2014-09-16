5d65eda8699411d58605d526c91110667132d27d
Beeno Tung <abbcc1241@yahoo.com.hk>
2014-09-17 03:14:09 +0800

    toned layout

--- gen/edu/hkcc/personalkcalmanagerhkcc/R.java
+++ gen/edu/hkcc/personalkcalmanagerhkcc/R.java
@@ -9,9 +9,12 @@ package edu.hkcc.personalkcalmanagerhkcc;
 
 public final class R {
     public static final class array {
-        public static final int navigation_drawer_titles=0x7f060000;
+        public static final int navigation_drawer_titles=0x7f070000;
     }
     public static final class attr {
+    }
+    public static final class color {
+        public static final int energycal_space_color=0x7f050000;
     }
     public static final class dimen {
         /**  Default screen margins, per the Android Design guidelines. 
@@ -23,13 +26,14 @@ public final class R {
          */
         public static final int activity_horizontal_margin=0x7f040000;
         public static final int activity_vertical_margin=0x7f040001;
+        public static final int energycal_colume_margin=0x7f040003;
         /** 
          Per the design guidelines, navigation drawers should be between 240dp and 320dp:
          https://developer.android.com/design/patterns/navigation-drawer.html
     
          */
         public static final int navigation_drawer_width=0x7f040002;
-        public static final int std_space_height=0x7f040003;
+        public static final int std_space_height=0x7f040004;
     }
     public static final class drawable {
         public static final int button_bg_blue_1=0x7f020000;
@@ -41,33 +45,32 @@ public final class R {
         public static final int start_screen_slogan=0x7f020006;
     }
     public static final class id {
-        public static final int LinearLayout1=0x7f090016;
-        public static final int TableLayout1=0x7f090003;
-        public static final int TableLayout_energycal_tablelayout_kcalrecord=0x7f09000f;
-        public static final int aboutyou_bmi_label=0x7f09000d;
-        public static final int aboutyou_button_calcuateBmi=0x7f09000c;
-        public static final int aboutyou_edittext_userage=0x7f090007;
-        public static final int aboutyou_edittext_userbmi=0x7f09000e;
-        public static final int aboutyou_edittext_userheight=0x7f090009;
-        public static final int aboutyou_edittext_username=0x7f090005;
-        public static final int aboutyou_edittext_userweight=0x7f09000b;
-        public static final int aboutyou_userage_label=0x7f090006;
-        public static final int aboutyou_userheight_label=0x7f090008;
-        public static final int aboutyou_username_label=0x7f090004;
-        public static final int aboutyou_userweight_label=0x7f09000a;
-        public static final int action_example=0x7f09001a;
-        public static final int action_settings=0x7f090019;
-        public static final int container=0x7f090001;
-        public static final int drawer_layout=0x7f090000;
-        public static final int imageView1=0x7f090017;
-        public static final int navigation_drawer=0x7f090002;
-        public static final int section_label=0x7f090015;
-        public static final int tableRow1=0x7f090010;
-        public static final int textView1=0x7f090014;
-        public static final int textView_energy_used=0x7f090013;
-        public static final int textView_times=0x7f090012;
-        public static final int textView_type_of_exercise=0x7f090011;
-        public static final int welcome_button_start=0x7f090018;
+        public static final int LinearLayout1=0x7f0a000f;
+        public static final int TableLayout1=0x7f0a0003;
+        public static final int aboutyou_bmi_label=0x7f0a000d;
+        public static final int aboutyou_button_calcuateBmi=0x7f0a000c;
+        public static final int aboutyou_edittext_userage=0x7f0a0007;
+        public static final int aboutyou_edittext_userbmi=0x7f0a000e;
+        public static final int aboutyou_edittext_userheight=0x7f0a0009;
+        public static final int aboutyou_edittext_username=0x7f0a0005;
+        public static final int aboutyou_edittext_userweight=0x7f0a000b;
+        public static final int aboutyou_userage_label=0x7f0a0006;
+        public static final int aboutyou_userheight_label=0x7f0a0008;
+        public static final int aboutyou_username_label=0x7f0a0004;
+        public static final int aboutyou_userweight_label=0x7f0a000a;
+        public static final int action_example=0x7f0a0019;
+        public static final int action_settings=0x7f0a0018;
+        public static final int container=0x7f0a0001;
+        public static final int drawer_layout=0x7f0a0000;
+        public static final int imageView1=0x7f0a0016;
+        public static final int navigation_drawer=0x7f0a0002;
+        public static final int section_label=0x7f0a0015;
+        public static final int tableRow1=0x7f0a0010;
+        public static final int textView1=0x7f0a0014;
+        public static final int textView_energy_used=0x7f0a0013;
+        public static final int textView_times=0x7f0a0012;
+        public static final int textView_type_of_exercise=0x7f0a0011;
+        public static final int welcome_button_start=0x7f0a0017;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
@@ -79,35 +82,35 @@ public final class R {
         public static final int fragment_welcome=0x7f030006;
     }
     public static final class menu {
-        public static final int global=0x7f080000;
-        public static final int main=0x7f080001;
+        public static final int global=0x7f090000;
+        public static final int main=0x7f090001;
     }
     public static final class string {
-        public static final int aboutyou_bmi_calc=0x7f05000f;
-        public static final int aboutyou_bmi_label=0x7f05000e;
-        public static final int aboutyou_calculating=0x7f050010;
-        public static final int aboutyou_pleasefillallinfo=0x7f050011;
-        public static final int aboutyou_userage_label=0x7f05000a;
-        public static final int aboutyou_userheight_label_cm=0x7f05000c;
-        public static final int aboutyou_userheight_label_m=0x7f05000b;
-        public static final int aboutyou_username_label=0x7f050009;
-        public static final int aboutyou_userweight_label=0x7f05000d;
-        public static final int action_example=0x7f050001;
-        public static final int action_settings=0x7f050002;
-        public static final int app_name=0x7f050000;
-        public static final int energycal_adding_record=0x7f050015;
-        public static final int energycal_date=0x7f050013;
-        public static final int energycal_energy_accum=0x7f050014;
-        public static final int energycal_energy_per_walk_stair=0x7f050016;
-        public static final int energycal_energy_type_hkcc_stair_walk=0x7f050017;
-        public static final int energycal_type_of_exercise=0x7f050012;
-        public static final int error_404=0x7f050006;
-        public static final int lets_go=0x7f050018;
-        public static final int loading=0x7f050005;
-        public static final int navigation_drawer_close=0x7f050004;
-        public static final int navigation_drawer_open=0x7f050003;
-        public static final int slogan=0x7f050007;
-        public static final int start=0x7f050008;
+        public static final int aboutyou_bmi_calc=0x7f06000f;
+        public static final int aboutyou_bmi_label=0x7f06000e;
+        public static final int aboutyou_calculating=0x7f060010;
+        public static final int aboutyou_pleasefillallinfo=0x7f060011;
+        public static final int aboutyou_userage_label=0x7f06000a;
+        public static final int aboutyou_userheight_label_cm=0x7f06000c;
+        public static final int aboutyou_userheight_label_m=0x7f06000b;
+        public static final int aboutyou_username_label=0x7f060009;
+        public static final int aboutyou_userweight_label=0x7f06000d;
+        public static final int action_example=0x7f060001;
+        public static final int action_settings=0x7f060002;
+        public static final int app_name=0x7f060000;
+        public static final int energycal_adding_record=0x7f060015;
+        public static final int energycal_date=0x7f060013;
+        public static final int energycal_energy_accum=0x7f060014;
+        public static final int energycal_energy_per_walk_stair=0x7f060016;
+        public static final int energycal_energy_type_hkcc_stair_walk=0x7f060017;
+        public static final int energycal_type_of_exercise=0x7f060012;
+        public static final int error_404=0x7f060006;
+        public static final int lets_go=0x7f060018;
+        public static final int loading=0x7f060005;
+        public static final int navigation_drawer_close=0x7f060004;
+        public static final int navigation_drawer_open=0x7f060003;
+        public static final int slogan=0x7f060007;
+        public static final int start=0x7f060008;
     }
     public static final class style {
         /** 
@@ -131,10 +134,10 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f070000;
+        public static final int AppBaseTheme=0x7f080000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f070001;
+        public static final int AppTheme=0x7f080001;
     }
 }
