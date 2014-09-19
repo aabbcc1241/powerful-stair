10c3c519050a26a1b5f15efc224b138f2c622ae0
Beeno Tung <abbcc1241@yahoo.com.hk>
2014-09-20 01:27:37 +0800

    removed some comment

--- src/edu/hkcc/personalkcalmanagerhkcc/MainActivity.java
+++ src/edu/hkcc/personalkcalmanagerhkcc/MainActivity.java
@@ -82,12 +82,22 @@ public class MainActivity extends Activity implements
 		inited = true;
 	}
 
+	public void initDrawer() {
+		mNavigationDrawerFragment = (NavigationDrawerFragment) getFragmentManager()
+				.findFragmentById(R.id.navigation_drawer);
+		mTitle = getTitle();
+
+		// Set up the drawer.
+		mNavigationDrawerFragment.setUp(R.id.navigation_drawer,
+				(DrawerLayout) findViewById(R.id.drawer_layout));
+	}
+
 	private void initListener() {
 		initListener_welcome();
 		initListener_aboutYou();
 		initListener_energyCal();
 
-		//myInit();
+		// myInit();
 	}
 
 	private void initListener_welcome() {
@@ -122,26 +132,6 @@ public class MainActivity extends Activity implements
 		}
 	}
 
-	/*
-	 * public void energyCal_addRecord() { int id = 99999999; TableRow tableRow
-	 * = new TableRow(this); tableRow.setLayoutParams(new TableRow.LayoutParams(
-	 * TableRow.LayoutParams.MATCH_PARENT, TableRow.LayoutParams.WRAP_CONTENT,
-	 * Gravity.CENTER_HORIZONTAL));
-	 * tableRow.setGravity(Gravity.CENTER_HORIZONTAL); tableRow.setId(id);
-	 * TextView textView1 = new TextView(this); TextView textView2 = new
-	 * TextView(this); TextView textView3 = new TextView(this); textView1
-	 * .setText(getString(R.string.energycal_energy_type_hkcc_stair_walk));
-	 * Calendar calendar = Calendar.getInstance(); int date =
-	 * calendar.get(Calendar.DATE); int month = calendar.get(Calendar.MONTH);
-	 * textView2.setText(date + "/" + month); int newCal = Integer
-	 * .valueOf(getString(R.string.energycal_energy_per_walk_stair));
-	 * energyCalFragment.accumCal(newCal);
-	 * textView3.setText(energyCalFragment.getCalAccumAsString());
-	 * tableRow.addView(textView1); tableRow.addView(textView2);
-	 * tableRow.addView(textView3);
-	 * energycal_tablelayout_energy.addView(tableRow); }
-	 */
-
 	private void myInit() {
 		initvar();
 		initListener();
@@ -160,13 +150,7 @@ public class MainActivity extends Activity implements
 		resolveIntent(getIntent());
 
 		initvar();
-		mNavigationDrawerFragment = (NavigationDrawerFragment) getFragmentManager()
-				.findFragmentById(R.id.navigation_drawer);
-		mTitle = getTitle();
-
-		// Set up the drawer.
-		mNavigationDrawerFragment.setUp(R.id.navigation_drawer,
-				(DrawerLayout) findViewById(R.id.drawer_layout));
+		initDrawer();
 	}
 
 	@Override
@@ -184,7 +168,7 @@ public class MainActivity extends Activity implements
 
 	@Override
 	public void onNavigationDrawerItemSelected(int position) {
-		//initvar();
+		// initvar();
 		myInit();
 		// update the main content by replacing fragments
 		FragmentManager fragmentManager = getFragmentManager();
@@ -196,19 +180,12 @@ public class MainActivity extends Activity implements
 	}
 
 	public void onSectionAttached(int sectinoNum) {
-
-		/*
-		 * switch (number) { case 1: mTitle = title_section_1; break; case 2:
-		 * mTitle = title_section_2; break; case 3: mTitle = title_section_3;
-		 * break; }
-		 */
-
 		mTitle = navigation_drawer_titles[sectinoNum];
 	}
 
 	public void switchSection(int sectionNum) {
-		//onSectionAttached(sectionNum);
-		//onNavigationDrawerItemSelected(sectionNum);
+		// onSectionAttached(sectionNum);
+		// onNavigationDrawerItemSelected(sectionNum);
 		mNavigationDrawerFragment.selectItem(sectionNum);
 		restoreActionBar();
 	}
