e3793886c86e6547fbf8554bd16beaf92bc593be
Beeno Tung <aabbcc1241@yahoo.com.hk>
2015-02-27 00:42:58 +0800

    moved cal calculation part from StairRecord to Maths

--- app/src/main/java/edu/hkcc/myutils/Maths.java
+++ app/src/main/java/edu/hkcc/myutils/Maths.java
@@ -1,14 +1,23 @@
 package edu.hkcc.myutils;
 
+import edu.hkcc.personalkcalmanagerhkcc.MainActivity;
+
 /**
  * Created by beenotung on 2/23/15.
  */
 public class Maths {
+    public static final float METs_STAIR_CLIMB = 3.5f;
+    public static final float STD_STAIR_HEIGHT = 2.0f;
     public static final float BLS_TO_KG = 0.453592f;
 
     public static float getBmi(float height, float weight) {
         float heightUnit = height < 100 ? 1 : 0.01f;
         float weightUnit = weight < 120 ? 1 : BLS_TO_KG;
         return (float) ((weight * weightUnit) / Math.pow(height * heightUnit, 2));
+    }
+
+    //METs x 3.5 x weight in kilograms ÷ 200 x duration in minutes
+    public static float calBurned(float height, float durationInMinutes) {
+        return METs_STAIR_CLIMB * 3.5f * MainActivity.currentActivity.aboutYouFragment.weight * MainActivity.currentActivity.aboutYouFragment.weightUnit / 200f * durationInMinutes * height / STD_STAIR_HEIGHT;
     }
 }
--- app/src/main/java/edu/hkcc/personalkcalmanagerhkcc/database/stairrecord/StairRecord.java
+++ app/src/main/java/edu/hkcc/personalkcalmanagerhkcc/database/stairrecord/StairRecord.java
@@ -1,13 +1,12 @@
 package edu.hkcc.personalkcalmanagerhkcc.database.stairrecord;
 
+import edu.hkcc.myutils.Maths;
 import edu.hkcc.personalkcalmanagerhkcc.MainActivity;
 
 /**
  * Created by beenotung on 2/26/15.
  */
 public class StairRecord {
-    public static final float METs_STAIR_CLIMB = 3.5f;
-    public static final float STD_STAIR_HEIGHT = 2.0f;
     public long id;
     public int stairPairId;
     public float calBurned;
@@ -15,7 +14,7 @@ public class StairRecord {
 
     public StairRecord(int stairPairId, float durationInMinutes) {
         this.stairPairId = stairPairId;
-        calBurned = calBurned(MainActivity.currentActivity.myDAO.stairPairDAOItem.getDistance(stairPairId), durationInMinutes);
+        calBurned = Maths.calBurned(MainActivity.currentActivity.myDAO.stairPairDAOItem.getDistance(stairPairId), durationInMinutes);
     }
 
     public StairRecord(long id, int stairPairId, float calBurned, int time) {
@@ -31,10 +30,5 @@ public class StairRecord {
 
     public String getWhereString() {
         return StairRecordDAOItem.TABLE_COL_ID + "=" + id;
-    }
-
-    //METs x 3.5 x weight in kilograms ÷ 200 x duration in minutes
-    public float calBurned(float height, float durationInMinutes) {
-        return METs_STAIR_CLIMB * 3.5f * MainActivity.currentActivity.aboutYouFragment.weight * MainActivity.currentActivity.aboutYouFragment.weightUnit / 200f * durationInMinutes * height / STD_STAIR_HEIGHT;
     }
 }
